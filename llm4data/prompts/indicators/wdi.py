037861096b2fd768c300e9f32df61aa471518bb8
Aivin V. Solatorio <avsolatorio@gmail.com>
2023-05-26 18:05:43 -0400

    Centralize the prompt interface for WDI
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- llm4data/prompts/indicators/wdi.py
+++ llm4data/prompts/indicators/wdi.py
@@ -2,9 +2,35 @@ from datetime import datetime
 from typing import Any
 from langchain.prompts.prompt import PromptTemplate
 from llm4data import indicator2name
+from llm4data.prompts.utils import get_prompt_manager
 
 
-class WDISQLPromptTemplate(PromptTemplate):
+class DatedPromptTemplate(PromptTemplate):
+    task_label = "wdi"
+    prompt_type = "zeros"
+
+    def format(self, **kwargs: Any) -> str:
+        if "now" not in kwargs:
+            kwargs["now"] = datetime.now().date()
+
+        return super().format(**kwargs)
+
+    def send_prompt(self, prompt: str, api_kwargs: dict = None, **kwargs: Any):
+        prompt_manager = get_prompt_manager(
+            self.task_label,
+            type=self.prompt_type,
+        )
+
+        return prompt_manager.send_prompt(
+            user_content=prompt,
+            user_template=self.format(**kwargs),
+            return_data=True,
+            api_kwargs=api_kwargs,
+        )
+
+
+class WDISQLPromptTemplate(DatedPromptTemplate):
+    task_label = "WDISQLPrompt"
 
     def __init__(self, input_variables=None, template=None):
         if template is None and input_variables is None:
@@ -25,13 +51,27 @@ class WDISQLPromptTemplate(PromptTemplate):
 
         super().__init__(input_variables=input_variables, template=template)
 
-    def format(self, **kwargs: Any) -> str:
-        if "now" not in kwargs:
-            kwargs["now"] = datetime.now().date()
 
-        return super().format(**kwargs)
+class WDIAPIPromptTemplate(DatedPromptTemplate):
+    task_label = "WDIAPIPrompt"
 
-    # def llm2sql(self, prompt):
-    #     now = datetime.now().date()
-    #     table = self.__tablename__
-    #     fields = ", ".join(self.__llm_fields__)
+    def __init__(self, input_variables=None, template=None):
+        if template is None and input_variables is None:
+            input_variables = ["now"]
+
+            template = (
+                "Current date: {now}\n\n"
+                "Generate the relevant API endpoint to answer the prompt: ```{{{{user_content}}}}```\n\n"
+                "- If access to the World Bank indicators API is not applicable for the prompt.\n"
+                "return: `None`\n"
+                "- Do not attempt to generate an API endpoint if the prompt does not ask for an indicator.\n"
+                "return: `None`\n"
+                "- Do not attempt to generate an API endpoint if the prompt implies conceptual information related to an indicator.\n"
+                "return: `None`\n"
+                "- Else\n"
+                "return: Your response must only be the completed and valid API URL: https://api.worldbank.org/v2/...&format=json\n\n"
+                "Always add `source=...` to the URL if multiple indicators are in the URL.\n\n"
+                "Do not explain."
+            )
+
+        super().__init__(input_variables=input_variables, template=template)
