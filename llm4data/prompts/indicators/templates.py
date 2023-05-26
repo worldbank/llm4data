99956e4981fb7b7e54a0bb1e014f84a29e6ebdf4
Aivin V. Solatorio <avsolatorio@gmail.com>
2023-05-26 16:40:14 -0400

    Implement IndicatorPromptTemplate
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- llm4data/prompts/indicators/templates.py
+++ llm4data/prompts/indicators/templates.py
@@ -1,20 +1,24 @@
 from langchain.prompts.prompt import PromptTemplate
 from llm4data import indicator2name
 
-class IndicatorTemplate:
+
+class IndicatorPromptTemplate:
     system = PromptTemplate(
-        input_variables=["indicator_name", "sample_data"],
-        template="""Pretend you are a genius economist. You respond in narrative manner.
+        input_variables=["indicator_name", "context_data"],
+        template=("Pretend you are a genius economist. You respond in narrative manner.\n\n"
+                  "You know this data{indicator_name}\n"
+                  "{context_data}\n\n"
+                  "Select only the relevant data when responding to the prompt.")
+    )
 
-You know this data{indicator_name}
-{sample_data}
-
-Select only the relevant data when responding to the prompt.""")
     user = PromptTemplate(
         input_variables=["prompt"],
-        template="""You always answer the prompt based on the data you know, and show the relevant numbers but not the raw data. You may respond in a different language if asked in the prompt. You approximate in millions or billions, and use correct units: ```{prompt}```""")
+        template=("You always answer the prompt based on the data you know, and show the relevant numbers but not the raw data."
+                  " You may respond in a different language if asked in the prompt."
+                  " You approximate in millions or billions, and use correct units: ```{prompt}```""")
+    )
 
-    def __init__(self, prompt: str, sample_data: str, indicator_code: str = None,  indicator_id: str = None):
+    def __init__(self, prompt: str = None, context_data: str = None, indicator_code: str = None,  indicator_id: str = None):
         if indicator_id is None:
             indicator_name = ":"
         else:
@@ -27,10 +31,16 @@ Select only the relevant data when responding to the prompt.""")
         self.indicator_id = indicator_id
         self.indicator_name = indicator_name
         self.prompt = prompt
-        self.sample_data = sample_data
+        self.context_data = context_data
 
-    def render(self, user: bool = False):
+    def format_prompt(self, user: bool = False):
         if user:
-            return self.user.render(prompt=self.prompt)
+            return self.user.format_prompt(prompt=self.prompt)
         else:
-            return self.system.render(indicator_name=self.indicator_name, sample_data=self.sample_data)
+            return self.system.format_prompt(indicator_name=self.indicator_name, context_data=self.context_data)
+
+    def build_message(self):
+        return [
+            dict(role="system", content=self.format_prompt(user=False).text),
+            dict(role="user", content=self.format_prompt(user=True).text)
+        ]
