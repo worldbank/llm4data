8311282a180c43f17124e121e0c337cfb801f072
Aivin V. Solatorio <avsolatorio@gmail.com>
2023-05-27 13:13:16 -0400

    Add conversational indicator template
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- llm4data/prompts/indicators/templates.py
+++ llm4data/prompts/indicators/templates.py
@@ -1,70 +1,41 @@
-from typing import Optional
-from langchain.prompts.prompt import PromptTemplate
+from typing import Optional, Any, List
+from pydantic import Extra
+from llm4data.prompts.base import DatedPrompt
 from llm4data import indicator2name
 
 
-class IndicatorPromptTemplate:
-    system = PromptTemplate(
-        input_variables=["indicator_name", "context_data"],
-        template=(
-            "Pretend you are a genius economist. You respond in narrative manner.\n\n"
-            "You know this data{indicator_name}\n"
-            "{context_data}\n\n"
-            "Select only the relevant data when responding to the prompt."
-        ),
+class IndicatorPrompt(DatedPrompt):
+    task_label = "IndicatorPrompt"
+
+    indicator_id: Optional[str] = None
+    indicator_code: Optional[str] = None
+    input_variables: List[str] = ["indicator_name", "context_data"]
+    template = (
+        "Pretend you are a genius economist. You respond in narrative manner.\n\n"
+        "You know this data{indicator_name}\n"
+        "{context_data}\n\n"
+        "Select only the relevant data when responding to the prompt.\n\n"
+        "You always answer the prompt based on the data you know, and show the relevant numbers but not the raw data."
+        " You may respond in a different language if asked in the prompt."
+        " You approximate in millions or billions, and use correct units: ```{{{{user_content}}}}```"
     )
 
-    user = PromptTemplate(
-        input_variables=["prompt"],
-        template=(
-            "You always answer the prompt based on the data you know, and show the relevant numbers but not the raw data."
-            " You may respond in a different language if asked in the prompt."
-            " You approximate in millions or billions, and use correct units: ```{prompt}```"
-            ""
-        ),
-    )
+    def format(self, **kwargs: Any) -> str:
+        if "indicator_name" not in kwargs:
+            if self.indicator_id is None:
+                indicator_name = ":"
+            else:
+                if self.indicator_code not in indicator2name:
+                    raise ValueError(
+                        f"indicator_code must be one of {list(indicator2name.keys())}"
+                    )
 
-    def __init__(
-        self,
-        prompt: Optional[str] = None,
-        context_data: Optional[str] = None,
-        indicator_code: Optional[str] = None,
-        indicator_id: Optional[str] = None,
-    ):
-        if indicator_id is None:
-            indicator_name = ":"
-        else:
-            if indicator_code not in indicator2name:
-                raise ValueError(
-                    f"indicator_code must be one of {list(indicator2name.keys())}"
-                )
+                indicator_name = indicator2name[self.indicator_code][self.indicator_id]
+                indicator_name = f" ({indicator_name}):"
 
-            indicator_name = indicator2name[indicator_code][indicator_id]
-            indicator_name = f" ({indicator_name}):"
+            kwargs["indicator_name"] = indicator_name
 
-        self.indicator_id = indicator_id
-        self.indicator_name = indicator_name
-        self.prompt = prompt
-        self.context_data = context_data
+        return super().format(**kwargs)
 
-    def format_prompt(self, user: bool = False, prompt: Optional[str] = None):
-        if user:
-            if not (self.prompt or prompt):
-                raise ValueError("prompt must be specified for user")
-
-            # Use the prompt passed in if it is not None, otherwise use the prompt specified in the constructor.
-            prompt = prompt or self.prompt
-            return self.user.format_prompt(prompt=prompt)
-        else:
-            return self.system.format_prompt(
-                indicator_name=self.indicator_name,
-                context_data=self.context_data,
-            )
-
-    def build_message(self, prompt: Optional[str] = None):
-        return [
-            dict(role="system", content=self.format_prompt(user=False).text),
-            dict(
-                role="user", content=self.format_prompt(user=True, prompt=prompt).text
-            ),
-        ]
+    def parse_response(self, response: dict, **kwargs: Any) -> Any:
+        return response["content"]
