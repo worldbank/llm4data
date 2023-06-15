a9eba1837f93e2e20e9b7d8d5953ef313fe6c447
Aivin V. Solatorio <avsolatorio@gmail.com>
2023-06-14 22:10:04 -0400

    Fix typing for qdrant file
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- llm4data/index/qdrant.py
+++ llm4data/index/qdrant.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional, Union
 from langchain.vectorstores import Qdrant
 import qdrant_client
 from qdrant_client.http import models
@@ -15,24 +16,29 @@ def collection_exists(collection_name: str) -> bool:
     return collection_name in [i.name for i in colls.collections]
 
 
-def get_index_client(path: str = None):
+def get_index_client(path: Optional[str] = None):
     global _CLIENT
     if _CLIENT is None:
-        if os.environ.get("QDRANT_URL") is not None:
-            url = os.environ.get("QDRANT_URL")
-            if os.environ.get("QDRANT_PORT") is not None:
-                url += f":{os.environ.get('QDRANT_PORT')}"
-            _CLIENT = qdrant_client.QdrantClient(url=url, prefer_grpc=False)
-        elif os.environ.get("QDRANT_PATH") is not None:
-            path = os.environ.get("QDRANT_PATH")
+        if path is not None:
             _CLIENT = qdrant_client.QdrantClient(path=path, prefer_grpc=True)
         else:
-            raise ValueError("QDRANT_URL or QDRANT_PATH not set in the environment")
+            url = os.environ.get("QDRANT_URL")
+            if url is not None:
+                port = os.environ.get("QDRANT_PORT")
+                if port is not None:
+                    url += f":{port}"
+                _CLIENT = qdrant_client.QdrantClient(url=url, prefer_grpc=False)
+            else:
+                path = os.environ.get("QDRANT_PATH")
+                if path is not None:
+                    _CLIENT = qdrant_client.QdrantClient(path=path, prefer_grpc=True)
+                else:
+                    raise ValueError("QDRANT_URL or QDRANT_PATH not set in the environment")
 
     return _CLIENT
 
 
-def get_index_collection(embeddings, path: str = None, recreate: bool = False):
+def get_index_collection(embeddings, path: Optional[str] = None, recreate: bool = False):
     client = get_index_client(path=path)
 
     if recreate:
@@ -58,17 +64,17 @@ def get_index_collection(embeddings, path: str = None, recreate: bool = False):
     )
 
 
-def get_docs_index(path: str = None, recreate: bool = False):
+def get_docs_index(path: Optional[str] = None, recreate: bool = False):
     return get_index_collection(get_docs_embeddings(), path=path, recreate=recreate)
 
 
-def get_indicators_index(path: str = None, recreate: bool = False):
+def get_indicators_index(path: Optional[str] = None, recreate: bool = False):
     return get_index_collection(
         get_indicators_embeddings(), path=path, recreate=recreate
     )
 
 
-def get_microdata_index(path: str = None, recreate: bool = False):
+def get_microdata_index(path: Optional[str] = None, recreate: bool = False):
     return get_index_collection(
         get_microdata_embeddings(), path=path, recreate=recreate
     )
